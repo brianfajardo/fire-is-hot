dfe1912520c6e7eeb830a6896074a2c759dc3661
Brian Fajardo <bmf5@hotmail.com>
2017-04-17 19:09:55 -0400

    Add life-cycle methods to handle routing based on authentication

--- client/components/Require_Auth.js
+++ client/components/Require_Auth.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 // Higher order component scaffold
 export default (ComposedComponent) => {
 	class Authentication extends Component {
+		// If the user is not authenticated, navigate
+		// back to home route
+		componentWillMount() {
+			if (!this.props.authenticated) {
+				this.context.router.history.push('/')
+			}
+		}
+
+		// Handling when the user is viewing the protected
+		// Resources path and logs out --> Will kick user from page
+		componentWillUpdate(nextProps) {
+			if (!nextProps.authenticated) {
+				this.context.router.history.push('/')
+			}
+		}
+
 		render() {
-			console.log(`isLoggedIn: ${this.props.authenticated}`)
 			return <ComposedComponent {...this.props} />
 		}
+	}
+
+	// Need to declare context types using PropTypes
+	Authentication.contextTypes = {
+		router: PropTypes.object
 	}
 
 	const mapStateToProps = state => ({
